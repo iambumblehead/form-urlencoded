b61cfc5658b512b0b09467877c737113d62877ea
Влад Пасечник <jump.e.r@yandex.ru>
2016-10-07 16:49:49 +0300

    Update form-urlencoded.js
    an array index number

--- form-urlencoded.js
+++ form-urlencoded.js
@@ -5,7 +5,7 @@
 // http://www.w3.org/TR/html5/forms.html#url-encoded-form-data
 // input: {one:1,two:2} return: '[one]=1&[two]=2'
 
-var formurlencoded = function (data, opts) {
+var formurlencoded = module.exports = function (data, opts) {
   // ES5 compatible version of `/[^ !'()~\*]/gu`, https://mothereff.in/regexpu
   var encodechar = new RegExp([
     '(?:[\0-\x1F"-&\+-\}\x7F-\uD7FF\uE000-\uFFFF]|',
