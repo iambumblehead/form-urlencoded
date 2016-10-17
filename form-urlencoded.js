da8e77cab73e6e1874e5742c11c86ab9ae55073f
Punk <jump.e.r@yandex.ru>
2016-10-18 02:03:56 +0300

    format code

--- form-urlencoded.js
+++ form-urlencoded.js
@@ -6,64 +6,66 @@
 // input: {one:1,two:2} return: '[one]=1&[two]=2'
 
 var formurlencoded = module.exports = function (data, opts) {
-  // ES5 compatible version of `/[^ !'()~\*]/gu`, https://mothereff.in/regexpu
-  var encodechar = new RegExp([
-    '(?:[\0-\x1F"-&\+-\}\x7F-\uD7FF\uE000-\uFFFF]|',
-    '[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|',
-    '(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])'
-  ].join(''), 'g');
+    // ES5 compatible version of `/[^ !'()~\*]/gu`, https://mothereff.in/regexpu
+    var encodechar = new RegExp([
+        '(?:[\0-\x1F"-&\+-\}\x7F-\uD7FF\uE000-\uFFFF]|',
+        '[\uD800-\uDBFF][\uDC00-\uDFFF]|[\uD800-\uDBFF](?![\uDC00-\uDFFF])|',
+        '(?:[^\uD800-\uDBFF]|^)[\uDC00-\uDFFF])'
+    ].join(''), 'g');
 
-  opts = typeof opts === 'object' ? opts : {};
-  
-  function encode (value) {
-    return String(value)
-      .replace(encodechar, encodeURIComponent)
-      .replace(/ /g, '+')
-      .replace(/[!'()~\*]/g, function (ch) {
-        return '%' + ch.charCodeAt().toString(16).slice(-2).toUpperCase();
-      });
-  }
+    opts = typeof opts === 'object' ? opts : {};
 
-  function keys (obj) {
-    var keys = Object.keys(obj);
-
-    return opts.sorted ? keys.sort() : keys;
-  }
-
-  function filterjoin (arr) {
-    return arr.filter(function (e) { return e; }).join('&');
-  }
-
-  function objnest (name, obj) {
-    return filterjoin(keys(obj).map(function (key) {
-      return nest(name + '[' + key + ']', obj[key]);
-    }));
-  }
-
-  function arrnest (name, arr) {
-    return arr.length ? filterjoin(arr.map(function (elem, index) {
-        return nest(name + '['+index+']', elem);
-    })) : encode(name + '[]');
-  }
-
-  function nest (name, value) {
-    var type = typeof value,
-        f = null;
-
-    if (value === f) {
-      f = opts.ignorenull ? f : encode(name) + '=' + f;
-    } else if (/string|number|boolean/.test(type)) {
-      f = encode(name) + '=' + encode(value);
-    } else if (Array.isArray(value)) {
-      f = arrnest(name, value);
-    } else if (type === 'object') {
-      f = objnest(name, value);
+    function encode(value) {
+        return String(value)
+            .replace(encodechar, encodeURIComponent)
+            .replace(/ /g, '+')
+            .replace(/[!'()~\*]/g, function (ch) {
+                return '%' + ch.charCodeAt().toString(16).slice(-2).toUpperCase();
+            });
     }
 
-    return f;
-  }
+    function keys(obj) {
+        var keys = Object.keys(obj);
 
-  return data && filterjoin(keys(data).map(function (key) {
-    return nest(key, data[key]);
-  }));
+        return opts.sorted ? keys.sort() : keys;
+    }
+
+    function filterjoin(arr) {
+        return arr.filter(function (e) {
+            return e;
+        }).join('&');
+    }
+
+    function objnest(name, obj) {
+        return filterjoin(keys(obj).map(function (key) {
+            return nest(name + '[' + key + ']', obj[key]);
+        }));
+    }
+
+    function arrnest(name, arr) {
+        return arr.length ? filterjoin(arr.map(function (elem, index) {
+            return nest(name + '[' + index + ']', elem);
+        })) : encode(name + '[]');
+    }
+
+    function nest(name, value) {
+        var type = typeof value,
+            f = null;
+
+        if (value === f) {
+            f = opts.ignorenull ? f : encode(name) + '=' + f;
+        } else if (/string|number|boolean/.test(type)) {
+            f = encode(name) + '=' + encode(value);
+        } else if (Array.isArray(value)) {
+            f = arrnest(name, value);
+        } else if (type === 'object') {
+            f = objnest(name, value);
+        }
+
+        return f;
+    }
+
+    return data && filterjoin(keys(data).map(function (key) {
+            return nest(key, data[key]);
+        }));
 };
