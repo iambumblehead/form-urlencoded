e7d402fb5d4c8cc3a87a54b19d56624c9b94acc9
chris <chris@bumblehead.com>
2020-07-18 13:48:54 -0700

    fix lint-related formatting

--- form-urlencoded.js
+++ form-urlencoded.js
@@ -1,5 +1,7 @@
 export default (data, opts = {}) => {
-  const { sorted, skipIndex, ignorenull, skipBracket, useDot, whitespace='+' } = opts;
+  const {
+    sorted, skipIndex, ignorenull, skipBracket, useDot, whitespace = '+'
+  } = opts;
 
   const encode = value => String(value)
     .replace(/[^ !'()~*]/gu, encodeURIComponent)
